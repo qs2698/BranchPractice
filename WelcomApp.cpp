30af1a5c2dba3c72c89aeee7a528d2c3a23757ce
qs2698 <qkrtkdgur152@gmail.com>
2025-07-09 21:57:18 +0900

    add hello

--- WelcomApp.cpp
+++ WelcomApp.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
+void addhello(){
+    std::cout << "hello!" << std::endl;
+}
+
 int main() {
     std::cout << "Welcome to the app!" << std::endl;
+    addhello();
     return 0;
 }
